0d89d82c7fbffb7d70a2738e6fa9c055307fa735
LeliaVlasenko <lelyakupina@gmail.com>
2026-06-15 21:47:25 +0200

    Initial Commit

---
+++ Dog Trainer/Dog_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dog_TrainerApp.swift
+//  Dog Trainer
+//
+//  Created by Lelia Vlasenko on 15/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Dog_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dog TrainerTests/Dog_TrainerTests.swift
@@ -0,0 +1,19 @@
+//
+//  Dog_TrainerTests.swift
+//  Dog TrainerTests
+//
+//  Created by Lelia Vlasenko on 15/06/2026.
+//
+
+import Testing
+@testable import Dog_Trainer
+
+struct Dog_TrainerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
